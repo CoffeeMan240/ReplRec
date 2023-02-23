d589abca12e1e04793974151299537765a4ac875
CoffeeMan <68038942+CoffeeMan240@users.noreply.github.com>
2023-02-23 08:34:31 -0700

    amog

---
+++ APIWSServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+public class APIWSServer
+{
+	public APIWSServer()
+	{
+		Console.WriteLine("(API) ONLINE!");
+
+	}
+	public void Listed()
+	{
+		var server = new HttpListener();
+		server.Prefixes.Add("http://+:2011");
+		for(; ; )
+		{
+			server.Start();
+			Console.WriteLine("(API) Listening...");
+            var context = server.GetContext();
+            string rawurl = context.Request.RawUrl;
+            string r = "[]";
+            string post;
+            byte[] bytepost;
+            Console.WriteLine("(API) REQUESTED " + rawurl + "!");
+            using (MemoryStream getpost = new MemoryStream())
+            {
+                context.Request.InputStream.CopyTo(getpost);
+                bytepost = getpost.ToArray();
+                post = Encoding.ASCII.GetString(bytepost);
+            }
+            switch(rawurl)
+            {
+                case "/j":
+                    Console.WriteLine("j");
+                    r = "jaded";
+                    break;
+                case "/api/versioncheck":
+                    r = "{\"ValidVersion\":true}";
+                    break;
+                
+            }
+        }
+	}
+}
--- main.cs
+++ main.cs
@@ -2,6 +2,7 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("RepelRec- an experimental replit server for RR");
+        new APIWSServer();
   }
 }
